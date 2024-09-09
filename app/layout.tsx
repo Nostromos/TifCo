18bedcc994e750e23005c8e63b804910dd2b2cb3
Michael Monaghan <Nostromos@users.noreply.github.com>
2024-09-09 15:39:34 -0400

    Metadata additions.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | TifCo Dashboard",
+    default: "TifCo Dashboard",
+  },
+  description: "The official dashboard for all fans & simps of TifCo.",
+  metadataBase: new URL('https://instagram.com/supsui'),
+};
 
 export default function RootLayout({
   children,
